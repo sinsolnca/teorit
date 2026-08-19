b15c14934bb7b982aebf53205b85d2dab7398fd7
sinsolnca <fed51774@gmail.com>
2026-08-19 23:05:54 +0300

    Fix TS error in Breadcrumbs

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -32,14 +32,12 @@ export function Breadcrumbs() {
     }
     if (index === 1 && segments[0] === 'subject') {
       const subject = getSubject(segment)
-      const title = subject?.title
-      const label: string = title !== undefined ? title : segment
+      const label = subject?.title ?? segment
       return { label, path, isLast }
     }
     if (index === 2 && segments[0] === 'topic') {
       const topic = getTopic(segments[1], segment)
-      const title = topic?.title
-      const label: string = title !== undefined ? title : segment
+      const label = topic?.title ?? segment
       return { label, path, isLast }
     }
     if (index === 1 && segments[0] === 'search') {
